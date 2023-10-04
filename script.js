2e5a791a115bef38f7bc26be978c99a9c7224d75
Antipoop <senkovskiy.nikita@hexly.ru>
2023-10-04 12:32:46 +0300

    62

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-$("button").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#us").offset().top},
-        'slow');
-});
+const clicks = () => {
+    $("button").click(function() {
+        $('html,body').animate({
+            scrollTop: $("#us").offset().top},
+            'slow');
+    });
+};
